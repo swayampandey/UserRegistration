278241400fc41e0da25c8d202489e3920e04a9a4
Swayam Prakash <swayam265@gmail.com>
2022-09-08 21:40:15 +0530

    [Swayam] : added functionaltiy to check valid first name in user registration

---
+++ UserRegistration/src/main/java/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class UserRegistrationMain {
+
+    public static void main(String[] args) {
+
+        UserRegistration firstName = new UserRegistration();
+        firstName.validFirstName();
+    }
+}
