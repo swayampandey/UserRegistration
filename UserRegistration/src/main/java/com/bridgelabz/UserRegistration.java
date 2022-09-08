bc02a4b846f7e320bb106dbc9cf9a8fbd6690856
Swayam Prakash <swayam265@gmail.com>
2022-09-09 00:05:27 +0530

    [Swayam] : added the functionality of valid password rule 2in user registration

--- UserRegistration/src/main/java/com/bridgelabz/UserRegistration.java
+++ UserRegistration/src/main/java/com/bridgelabz/UserRegistration.java
@@ -9,7 +9,7 @@ public class UserRegistration {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter password :");
         String password = sc.next();
-        Pattern P = Pattern.compile("[a-zA-Z0-9]{8}");
+        Pattern P = Pattern.compile("[A-Z][a-zA-Z0-9]{7}");
         Matcher m = P.matcher(password);
         if(m.find() && m.group().equals(password)){
             System.out.println("Valid password");
